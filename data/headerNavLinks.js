d9e4d8da5d11dcd020d068f49f68636d744e412b
リイノ Lin <sorphwer@gmail.com>
2025-08-06 11:00:34 +0800

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,7 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'archive' },
   { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
-  { href: '/profile', title: 'Profile' },
+  // { href: '/profile', title: 'Profile' },
   { href: '/about', title: 'About' },
 ]
 
